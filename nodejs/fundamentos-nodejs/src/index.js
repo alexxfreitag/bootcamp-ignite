ffab4cd35d665c48238eb365b5ef0455585a2777
Alex Eduardo Freitag <alex.f@quantich.com.br>
2021-03-09 19:23:15 -0300

    Creates routes for every HTTP method

--- nodejs/fundamentos-nodejs/src/index.js
+++ nodejs/fundamentos-nodejs/src/index.js
@@ -2,10 +2,47 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({
-    message: 'Hello world!'
-  });
+app.get('/courses', (request, response) => {
+  return response.json([
+    "Course 1",
+    "Course 2",
+    "Course 3"
+  ]);
+})
+
+app.post('/courses', (request, response) => {
+  return response.json([
+    "Course 1",
+    "Course 2",
+    "Course 3",
+    "Course 4",
+  ]);
+})
+
+app.put('/courses/:id', (request, response) => {
+  return response.json([
+    "Course 6",
+    "Course 2",
+    "Course 3",
+    "Course 4",
+  ]);
+})
+
+app.patch('/courses/:id', (request, response) => {
+  return response.json([
+    "Course 6",
+    "Course 7",
+    "Course 3",
+    "Course 4",
+  ]);
+})
+
+app.delete('/courses/:id', (request, response) => {
+  return response.json([
+    "Course 6",
+    "Course 7",
+    "Course 4",
+  ]);
 })
 
 app.listen(3333);
