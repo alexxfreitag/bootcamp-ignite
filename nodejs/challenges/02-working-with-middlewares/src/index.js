2c4de70353df683efa6c7564d21b77e888068443
Alex Eduardo Freitag <alex.f@quantich.com.br>
2021-04-01 23:43:13 -0300

    Finishes 'working with middlewares' challenge

--- nodejs/challenges/02-working-with-middlewares/src/index.js
+++ nodejs/challenges/02-working-with-middlewares/src/index.js
@@ -10,19 +10,69 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found.' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro) return next();
+
+  if (user.todos.length >= 10) {
+    return response.status(403).json({ error: 'User already has ten todos.' });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id: todoId } = request.params;
+
+  if (!validate(todoId)) {
+    return response.status(400).json({ error: 'Invalid id, must be an uuid. '});
+  }
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found.' });
+  }
+
+  const todo = user.todos.find(todo => todo.id === todoId);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found.' });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id: userId } = request.params;
+
+  const user = users.find(user => user.id === userId);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found.' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
