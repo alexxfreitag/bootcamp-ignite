8bc82947184493a235bfe33c9b1f00187b3f7ce4
Alex Eduardo Freitag <alex.f@quantich.com.br>
2021-05-04 21:10:22 -0300

    Removes unused import

--- nodejs/04-rentalx/src/modules/cars/useCases/createSpecification/index.ts
+++ nodejs/04-rentalx/src/modules/cars/useCases/createSpecification/index.ts
@@ -1,5 +1,4 @@
 import { SpecificationsRepository } from '../../repositories/implementations/SpecificationsRepository';
-import { CreateCategoryController } from '../createCategory/CreateCategoryController';
 import { CreateSpecificationController } from './CreateSpecificationController';
 import { CreateSpecificationUseCase } from './CreateSpecificationUseCase';
 
